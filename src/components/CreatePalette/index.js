d9d3ebc68c0c54e9a6eba174439905ef2ad0f2e5
xavierigor <42844077+xavierigor@users.noreply.github.com>
2019-08-19 22:06:35 -0300

    Connect color picker to Button

--- src/components/CreatePalette/index.js
+++ src/components/CreatePalette/index.js
@@ -3,40 +3,126 @@ import { Link } from "react-router-dom";
 
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Button from "@material-ui/core/Button";
+import { ChromePicker } from "react-color";
+import chroma from "chroma-js";
+
+import ColorBox from "../ColorBox";
 
 import { Container } from "./styles";
 
-const drawerWidth = 240;
+const drawerWidth = 340;
 
-export default function CreatePalette() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  title: {
+    flexGrow: 1,
+    fontWeight: 500,
+    fontSize: 18
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  hide: {
+    display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "space-around"
+  },
+  content: {
+    flexGrow: 1,
+    paddingLeft: theme.spacing(0),
+    paddingight: theme.spacing(0),
+    paddingBottom: theme.spacing(0),
+    paddingTop: 1,
+    // padding: theme.spacing(0),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  }
+}));
+
+export default function PersistentDrawerLeft() {
   const classes = useStyles();
+  // const theme = useTheme();
   const [open, setOpen] = useState(false);
+  const [currentColor, setCurrentColor] = useState("#fff");
+  const [colors, setColors] = useState(["purple", "#ece"]);
+
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose() {
+    setOpen(false);
+  }
 
   return (
-    <div className={classes.root}>
-      <Container>
+    <Container>
+      <div className={classes.root}>
         <CssBaseline />
         <AppBar
+          color="default"
           position="fixed"
           className={clsx(classes.appBar, {
             [classes.appBarShift]: open
           })}
-          color="default"
         >
           <Toolbar>
             <IconButton
               color="inherit"
               aria-label="open drawer"
-              onClick={() => setOpen(true)}
+              onClick={handleDrawerOpen}
               edge="start"
               className={clsx(classes.menuButton, open && classes.hide)}
             >
@@ -63,10 +149,36 @@ export default function CreatePalette() {
           }}
         >
           <div className={classes.drawerHeader}>
-            <IconButton onClick={() => setOpen(false)}>
+            <IconButton onClick={handleDrawerClose}>
               <ChevronLeftIcon />
             </IconButton>
           </div>
+          <Divider />
+
+          <Typography variant="h5">Design Your Palette</Typography>
+
+          <div className={classes.buttons}>
+            <Button variant="contained" color="secondary">
+              Clear Palette
+            </Button>
+            <Button variant="contained" color="primary">
+              Random Color
+            </Button>
+          </div>
+          <ChromePicker
+            color={currentColor}
+            onChangeComplete={newColor => setCurrentColor(newColor.hex)}
+          />
+          <Button
+            style={{
+              backgroundColor: currentColor,
+              color: chroma(currentColor).luminance() <= 0.3 ? "#fff" : "#000"
+            }}
+            variant="contained"
+            onClick={() => setColors([...colors, currentColor])}
+          >
+            Add Color
+          </Button>
         </Drawer>
         <main
           className={clsx(classes.content, {
@@ -74,69 +186,20 @@ export default function CreatePalette() {
           })}
         >
           <div className={classes.drawerHeader} />
+
+          {colors.map(color => (
+            <ColorBox
+              name="New Color"
+              background={color}
+              showLink={false}
+              isTall={false}
+              key={color}
+            >
+              {color}
+            </ColorBox>
+          ))}
         </main>
-      </Container>
-    </div>
+      </div>
+    </Container>
   );
 }
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  title: {
-    flexGrow: 1,
-    fontWeight: 500,
-    fontSize: 18
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  }
-}));
